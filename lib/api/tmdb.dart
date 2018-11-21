64f9cd5b0b0d8e32301054493bde99f59ab444aa
NBTX <nbtx@telegenic.me>
2018-11-21 00:09:41 +0000

    Oops.

--- lib/api/tmdb.dart
+++ lib/api/tmdb.dart
@@ -3,4 +3,6 @@ import 'package:kamino/vendor/config/official.dart';
 const root_url = "https://api.themoviedb.org/3";
 const image_cdn = "https://image.tmdb.org/t/p/w500";
 
-const default_arguments = "?api_key=$tvmb_api_key&language=en-US";
+/// You will need to define [tmdb_api_key] in your vendor configuration file.
+/// Check our documentation for more information.
+const default_arguments = "?api_key=$tmdb_api_key&language=en-US";
