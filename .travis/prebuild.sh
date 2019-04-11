4b6bafd2a9aec588171953413906e3cd8e00ee45
NBTX <nbtx@telegenic.me>
2019-04-11 02:05:50 +0100

    Include Travis build URL

--- .travis/prebuild.sh
+++ .travis/prebuild.sh
@@ -4,7 +4,7 @@ TIMESTAMP=$(date -u +%FT%TZ)
 
 WEBHOOK_DATA='{
     "username": "ApolloTV (Travis)",
-    "content": "A build has started.\n\nJob #'"$TRAVIS_JOB_NUMBER"' (Build #'"$TRAVIS_BUILD_NUMBER"') '"$STATUS_MESSAGE"' - '"$TRAVIS_REPO_SLUG"'\n\n'"$COMMIT_SUBJECT"'"
+    "content": "A build has started.\n\n[Job #'"$TRAVIS_JOB_NUMBER"' (Build #'"$TRAVIS_BUILD_NUMBER"') '"$STATUS_MESSAGE"' - '"$TRAVIS_REPO_SLUG"'\n\n'"$COMMIT_SUBJECT"']('"$TRAVIS_BUILD_WEB_URL"')"
 }'
 
 (curl --fail --progress-bar -H Content-Type:application/json -d "$WEBHOOK_DATA" "$1" \
